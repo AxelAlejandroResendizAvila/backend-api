c7bbd305f7525da6290bd87cf6b638be2688cf2c
dev <dev@2806-103e-0027-2ee3-2ad2-6ba5-1902-263c.ipv6.infinitum.net.mx>
2026-02-09 23:11:01 -0600

    Commit inicial

---
+++ src/controllers/externalController.js
@@ -0,0 +1,38 @@
+const pool = require('../config/db');
+
+const poblarProductos = async (request, response) => {
+    try {
+        // Fetch FakeStoreApi
+        const apiFetch = await fetch('http://fakestoreapi.com/products');
+        const products = await apiFetch.json();
+
+        let inserciones = 0;
+        // Destructurar el objeto
+        for(const product of products){
+            const { title, price, description, image} = product;
+
+            const stock = Math.floor(Math.random() * 50) + 1;
+
+            const query = `
+                INSERT INTO productos
+                (nombre, precio, stock, descripcion, imagen_url)
+                VALUES ($1, $2, $3, $4, $5)
+            `
+
+            await pool.query(query, [title, price, stock, description, image]);
+
+            inserciones++;
+        }
+        response.status(200).json(
+            {
+                mensaje: "Carga masiva exitosa", 
+                cantidad: inserciones
+            }
+        );
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        response.status(500).json({error: error.message})
+    }
+};
+
+module.exports = { poblarProductos };
